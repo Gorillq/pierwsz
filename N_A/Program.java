6dcad9c530be7f5203f286aea830fbce3256e6a0
Gorillq <kamil.kownacki3@gmail.com>
2023-10-21 00:11:10 +0200

    crazy

---
+++ N_A/Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Program {
+    double a, b;
+    public static void main(String[] args) {
+        Scanner javain = new Scanner(System.in);
+        System.out.println("HI, CHOOSE OPTION:\n1.Add\n2.Minus\n3.Multi\n4.Divi");
+        int option = javain.nextInt();
+        System.exit(0);
+    }
+
+}
---
+++ N_A/Something.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Something {
+    private int a, b;
+    void add() {
+
+    }
+    public static void main(String[] args) {
+        myObj = new Something();
+        myObj2 = new Something();
+        Scanner javajava = new Scanner(System.in);
+        int inputone = javajava.nextInt();
+        int inputtwo = javajava.nextInt();
+        int inputoption = javajava.nextInt();
+        switch(inputoption) {
+            case 1:
+
+                add(inputone, inputtwo);
+                break;
+        }
+    }
+}
